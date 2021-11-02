3b54e87e52e32e3126ff921d0adbba7553391f7c
Yakov Kantor <80357569+yakov10105@users.noreply.github.com>
2021-11-02 20:36:23 +0200

    server-validation

---
+++ Chat_App/Services/Auth/Exeptions/EmailAlreadyExistExeption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_App.Services.Auth.Exeptions
+{
+    public class EmailAlreadyExistExeption : Exception
+    {
+        public EmailAlreadyExistExeption(string message) : base(message)
+        {
+        }
+    }
+}
