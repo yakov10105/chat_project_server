f2fd20b166d006f2260855b9afb4760addf01a1e
Yakov Kantor <80357569+yakov10105@users.noreply.github.com>
2021-11-17 08:02:39 +0200

    c

--- Chat_App/Controllers/GameController.cs
+++ Chat_App/Controllers/GameController.cs
@@ -14,8 +14,8 @@ namespace Chat_App.Controllers
     [ApiController]
     public class GameController : ControllerBase
     {
-        [HttpGet("board")]
-        public IActionResult GetBoard()
+        public GameService gameService;
+        public GameController()
         {
             // Checkers in start position
             // Boardfield     0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 -  E  G2 G1
@@ -23,21 +23,31 @@ namespace Chat_App.Controllers
             int[] p2Array = { 0, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0 };
 
             var reciver = new User() { Id = 222, UserName = "ggg333" };
-            var sender = new User() { Id = 222, UserName = "ggg333" };
+            var sender = new User() { Id = 333, UserName = "mmm333" };
 
-            var gameService = new GameService(sender, reciver);
+            gameService = new GameService(sender, reciver);
             gameService.InitBoardState(p1Array, p2Array);
             gameService.StartGame();
             gameService.RollDices();
+        }
+        [HttpGet("board")]
+        public IActionResult GetBoard()
+        {
 
-            //var a = gameService.GameBoard.PossibleMoves[0];
-            //var move = new Move() { from = a.From.GetPosition(), to = a.To.GetPosition() };
-            //gameService.MakeMove(move);
+            var a = gameService.GameBoard.PossibleMoves[0];
+            var move = new Move() { from = a.From.GetPosition(), to = a.To.GetPosition() };
+            gameService.MakeMove(move);
 
             //return Ok(gameService.GameBoard);
             //return Ok(gameService.GameBoard.BoardFields[0]);
             //return Ok(gameService.GameBoard.BoardFields[0].GetCheckers());
             return Ok(gameService.GetGameBoard());
+        }
+        [HttpGet("get-moves")]
+        public IActionResult GetPossibleMoves(int pos)
+        {
+            var lst = gameService.GetPossibleMoveFromPosition(pos);
+            return Ok(lst);
         }
     }
 }
